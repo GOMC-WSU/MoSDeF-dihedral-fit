ac76c6966a269f979365ad06d5fd81a1d502e6ab
Brad Crawford <65550266+bc118@users.noreply.github.com>
2023-02-14 11:13:18 -0500

    Update __init__.py

--- mosdef_dihedral_fit/__init__.py
+++ mosdef_dihedral_fit/__init__.py
@@ -1,5 +1,5 @@
 """ MoSDeF-dihedral-fit: A simple software package to fit dihedrals using the MoSDeF."""
-from MoSDeF_dihedral_fit.dihedral_fit.fit_dihedral_with_gomc import fit_dihedral_with_gomc
+from mosdef_dihedral_fit.dihedral_fit.fit_dihedral_with_gomc import fit_dihedral_with_gomc
 
 
 __version__ = "0.0.0"
