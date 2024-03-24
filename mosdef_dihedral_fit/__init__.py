dbc288039c01eaf2905787d4cac975e9165f3086
bc118 <crawfordb118@gmail.com>
2024-03-24 18:20:23 -0400

    changed the git __init__.py file error

--- mosdef_dihedral_fit/__init__.py
+++ mosdef_dihedral_fit/__init__.py
@@ -1,8 +1,6 @@
 """ MoSDeF-dihedral-fit: A simple software package to fit dihedrals using the MoSDeF."""
 
-from mosdef_dihedral_fit.dihedral_fit.fit_dihedral_with_gomc import (
-    fit_dihedral_with_gomc,
-)
+from mosdef_dihedral_fit.dihedral_fit import fit_dihedral_with_gomc
 from mosdef_dihedral_fit.utils.math_operations import dihedral_angle
 
 __version__ = "0.0.0"
