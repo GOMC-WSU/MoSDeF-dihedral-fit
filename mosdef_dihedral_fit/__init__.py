63e649b8ce63e7e8729ee2576f29fe9e82c83560
bc118 <crawfordb118@gmail.com>
2024-03-24 22:48:19 -0400

    fix init.py

--- mosdef_dihedral_fit/__init__.py
+++ mosdef_dihedral_fit/__init__.py
@@ -2,7 +2,7 @@
 
 from mosdef_dihedral_fit.dihedral_fit.fit_dihedral_with_gomc import (
   fit_dihedral_with_gomc, 
-)
+) 
 from mosdef_dihedral_fit.utils.math_operations import dihedral_angle
 
 __version__ = "0.0.2"
