54d1185fabc04e15fb55679e31486c7e93c80b37
Brad Crawford <65550266+bc118@users.noreply.github.com>
2024-08-03 20:22:01 -0400

    Update test_fit_dihedral_with_gomc.py
    Changed ‘GOMC_binary_path’ from None to “” .  
    
    Doing this to see if it changes

--- mosdef_dihedral_fit/tests/test_fit_dihedral_with_gomc.py
+++ mosdef_dihedral_fit/tests/test_fit_dihedral_with_gomc.py
@@ -15,7 +15,7 @@ from mosdef_dihedral_fit.tests.base_test import BaseTest
 # automated GitHub test binary path.
 # The automated GitHub test filename and location = "MoSDeF-dihedral-fit/.github/workflows/CI.yml"
 gomc_binary_directory = (
-    None  # set GOMC binary path here if you want to manually specify it
+    ""  # set GOMC binary path here if you want to manually specify it
 )
 # User changable variable, if the binary is not in the environment variable "PATH"
 # Examples
