63247da0753d825f590885126860702f4feaa61b
bc118 <crawfordb118@gmail.com>
2024-10-27 07:38:31 -0400

    Bump to version 0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 #########################################
 NAME = "MoSDeF-dihedral-fit"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
